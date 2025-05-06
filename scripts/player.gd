4718a57ee774eefd9557241f2471757bae9a2cbc
Jani Pekola <jani.pekola@gmail.com>
2025-05-06 13:51:09 +0300

    better player controller

--- scripts/player.gd
+++ scripts/player.gd
@@ -5,7 +5,7 @@ extends CharacterBody3D
 @onready var raycast: RayCast3D = $head/Camera3D/RayCast3D
 
 var currentSpeed = 5.0
-
+var look_rotation = Vector2()
 const WALK_SPEED = 5.0
 const SPRINT_SPEED = 8.0
 const JUMP_VELOCITY = 4.5
@@ -14,14 +14,27 @@ const MOUSE_SENS = 0.003
 func _ready() -> void:
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 
+#func _input(event):
+#	if event is InputEventMouseMotion:
+#		rotation.y += (-event.relative.x * MOUSE_SENS)
+#		rotation.x += (-event.relative.y * MOUSE_SENS)
+#		rotation.x = clamp(rotation.x, -90, 90)
+#	
+#	if event.is_action_pressed("ui_cancel"):
+#		get_tree().quit()
+
 func _input(event):
 	if event is InputEventMouseMotion:
-		rotation.y += (-event.relative.x * MOUSE_SENS)
-		rotation.x += (-event.relative.y * MOUSE_SENS)
-		rotation.x = clamp(rotation.x, -90, 90)
-	
+		look_rotation.x -= event.relative.x * MOUSE_SENS
+		look_rotation.y -= event.relative.y * MOUSE_SENS
+		look_rotation.y = clamp(look_rotation.y, -1.5, 1.5)  # Prevent flipping
+
+		rotation.y = look_rotation.x
+		head.rotation.x = look_rotation.y
+
 	if event.is_action_pressed("ui_cancel"):
 		get_tree().quit()
+
 
 func _physics_process(delta: float) -> void:
 	
